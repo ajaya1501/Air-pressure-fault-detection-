a260f8a02a44dec44a7b31a66297928d7737c382
ajaya1501 <ak9734279@gmail.com>
2022-12-11 15:29:39 +0530

    completed

--- sensor/utils.py
+++ sensor/utils.py
@@ -93,4 +93,4 @@ def load_numpy_array_data(file_path: str) -> np.array:
         with open(file_path, "rb") as file_obj:
             return np.load(file_obj)
     except Exception as e:
-        raise SensorException(e, sys) from e
+        raise SensorException(e, sys) from 
